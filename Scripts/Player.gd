f37a47e0e51d9a5a26b8f0cb41822d4cff36f5e2
NxKuma <neilisraelko01@gmail.com>
2024-09-27 14:17:28 +0800

    Started Handling Going down One Way Tile

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -7,12 +7,13 @@ var direction: int
 
 
 @onready var debug: CanvasLayer = $"../DebugLayer"
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
 
 @export var walk_speed: int = 350
 @export var run_speed: int = 500
 @export var jump_velocity: float = -600.0
 
-@export_range(1,2) var gravity_level: float = 1
+@export_range(1,8) var gravity_level: float = 1
 @export_range(0,1) var jump_deccelerate: float = 0.5
 @export_range(0,1) var deccelerate: float = 0.1
 @export_range(0,1) var accelerate: float = 0.1
@@ -24,29 +25,32 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	elif not is_on_floor() and velocity.y > 0:
+		velocity.y += gravity * delta * velocity.y
 	else:
 		jump_left = 2
-
+#-----------------------------------------------------------------------------------------------
 	# Handle jump.
-	if Input.is_action_just_pressed("Space") and (is_on_floor() or is_on_wall()):
-		velocity.y = jump_velocity
-		jump_left -= 1
-	elif Input.is_action_just_pressed("Space") and jump_left > 0:
-		velocity.y = jump_velocity
-		jump_left -= 1
-	
-	if Input.is_action_just_released("Space") and velocity.y < 0:
-		velocity.y *= jump_deccelerate
-	
+	if !Input.is_action_pressed("Down"):
+		if Input.is_action_just_pressed("Space") and (is_on_floor() or is_on_wall()):
+			velocity.y = jump_velocity
+			jump_left -= 1
+		elif Input.is_action_just_pressed("Space") and jump_left > 0:
+			velocity.y = jump_velocity
+			jump_left -= 1
+		
+		if Input.is_action_just_released("Space") and velocity.y < 0:
+			velocity.y *= jump_deccelerate
+#-----------------------------------------------------------------------------------------------
 	#Handle Running
 	var speed:int
 	var speed_limit:int
 	if Input.is_action_pressed("Run"):
 		speed = run_speed
-		speed_limit = 600
+		speed_limit = 800
 	else:
 		speed = walk_speed
-		speed_limit = 480
+		speed_limit = 560
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -66,8 +70,12 @@ func _physics_process(delta):
 		if velocity.x < speed_limit:
 			velocity.x += direction * speed *accelerate
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed*deccelerate)
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, speed*deccelerate)
 		
 	move_and_slide()
 	
 #-----------------------------------------------------------------------------------------------
+
+func _input(event: InputEvent):
+	pass
