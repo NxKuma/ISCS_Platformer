abd9c1a49cd27439a914aa88f66fc58ae97c876d
Mikh Villanueva <mikhael.villanueva@student.ateneo.edu>
2024-09-29 03:27:45 +0800

    Fixed Disappearing Platform
    platform sprite now reappears and cannot be reactivated if its still missing

--- Scripts/DisappearingPlatform.gd
+++ Scripts/DisappearingPlatform.gd
@@ -2,9 +2,10 @@ extends StaticBody2D
 
 @onready var timer: Timer = $Timer
 @onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var sprite_2d: Sprite2D = $Sprite2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
+	if body.name == "Player" and self.get_modulate() != Color(1,1,1,0):
 		timer.start()
 	
 func _on_timer_timeout() -> void:
@@ -12,5 +13,7 @@ func _on_timer_timeout() -> void:
 	tween.tween_property(self, "modulate:a", 0, 0.4) #change the disappear speed by changing the 0.5
 	collision_shape_2d.disabled = true
 	await get_tree().create_timer(3.0).timeout
-	tween.tween_property(self, "modulate:a", 1000, 0.4)
+	tween.kill()
+	tween = create_tween()
+	tween.tween_property(self, "modulate:a", 1, 0.4)
 	collision_shape_2d.disabled = false
